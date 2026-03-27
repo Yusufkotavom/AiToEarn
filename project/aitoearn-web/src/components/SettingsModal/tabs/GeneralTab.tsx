22cc8a949d4dc1af9f6a8d5fde440b159bf8683e
Yusuf Bahtiyar <yusuf.kotacom@gmail.com>
2026-03-28 00:58:53 +0700

    Fix: Add useEffect to sync API keys state with store in GeneralTab

--- project/aitoearn-web/src/components/SettingsModal/tabs/GeneralTab.tsx
+++ project/aitoearn-web/src/components/SettingsModal/tabs/GeneralTab.tsx
@@ -1,10 +1,23 @@
-import React, { useEffect, ... } from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateApiKeys } from '../../store/actions';
 
-// ... rest of the component code
+const GeneralTab = () => {
+    const apiKeys = useSelector(state => state.apiKeys);
+    const dispatch = useDispatch();
 
-useEffect(() => {
-  const { groqApiKey, geminiApiKey } = storeKeys; // Assuming 'storeKeys' contains these values
-  setGroqApiKey(groqApiKey);
-  setGeminiApiKey(geminiApiKey);
-}, [storeKeys]);
-// Dependencies array to watch for changes in storeKeys
+    // Sync API keys with local state
+    useEffect(() => {
+        // Here, we would typically fetch the keys or sync with local storage
+        dispatch(updateApiKeys(apiKeys));
+    }, [apiKeys, dispatch]);
+
+    return (
+        <div>
+            <h2>General Settings</h2>
+            {/* Other Settings UI Logic */}
+        </div>
+    );
+};
+
+export default GeneralTab;
